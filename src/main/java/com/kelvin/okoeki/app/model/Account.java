44e5fe056e36ef18b96dc0c857894e47f9dc481a
Kelvin Okoeki <annabelogbuagu@Annabels-MacBook-Air.local>
2023-03-29 02:43:50 +0100

    Added Models to be used.

--- src/main/java/com/kelvin/okoeki/app/model/Account.java
+++ src/main/java/com/kelvin/okoeki/app/model/Account.java
@@ -1,2 +1,28 @@
-package com.kelvin.okoeki.app.model;public class Account {
+package com.kelvin.okoeki.app.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long accountId;
+
+    private String accountNumber;
+
+    private String firstName;
+
+    private String lastName;
+
+    private BigDecimal currentBalance;
+
+    private Timestamp createdTime;
 }
--- src/main/java/com/kelvin/okoeki/app/model/Transaction.java
+++ src/main/java/com/kelvin/okoeki/app/model/Transaction.java
@@ -1,2 +1,25 @@
-package com.kelvin.okoeki.app.model;public class Transaction {
+package com.kelvin.okoeki.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "transaction")
+@AllArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long transactionId;
+
+    private String accountNumber;
+
+    private BigDecimal transactionAmount;
+
+    private Timestamp transactionDateTime;
 }
