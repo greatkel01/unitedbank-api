b035f12b7b4029d7f026651963f7a33804d2ff93
Kelvin Okoeki <annabelogbuagu@Annabels-MacBook-Air.local>
2023-03-29 02:48:25 +0100

    Created the service for the below funtionality - Account Creation - Fund Transfer - Cash deposit - Cash withdrawal - View Account - View all Accounts

--- src/main/java/com/kelvin/okoeki/app/service/AccountService.java
+++ src/main/java/com/kelvin/okoeki/app/service/AccountService.java
@@ -1,2 +1,23 @@
-package com.kelvin.okoeki.app.service;public class AccountService {
+package com.kelvin.okoeki.app.service;
+
+import com.kelvin.okoeki.app.dtos.AccountStatement;
+import com.kelvin.okoeki.app.dtos.requests.TransferBalanceRequest;
+import com.kelvin.okoeki.app.model.Account;
+import com.kelvin.okoeki.app.model.Transaction;
+
+import java.util.List;
+
+public interface  AccountService {
+    List<Account> findAll();
+    Account save(Account account);
+    Transaction transferFunds(
+            TransferBalanceRequest transferBalanceRequest
+    );
+    AccountStatement getStatement(String accountNumber);
+
+    Account findByAccountNumber(String accountNumber);
+
+    public boolean withdraw(String accountNumber, double amount);
+
+    public boolean deposit(String accountNumber, double amount);
 }
--- src/main/java/com/kelvin/okoeki/app/service/impl/AccountServiceImpl.java
+++ src/main/java/com/kelvin/okoeki/app/service/impl/AccountServiceImpl.java
@@ -1,2 +1,90 @@
-package com.kelvin.okoeki.app.service.impl;public class AccountServiceImpl {
+package com.kelvin.okoeki.app.service.impl;
+
+import com.kelvin.okoeki.app.dtos.AccountStatement;
+import com.kelvin.okoeki.app.dtos.requests.TransferBalanceRequest;
+import com.kelvin.okoeki.app.model.Account;
+import com.kelvin.okoeki.app.model.Transaction;
+import com.kelvin.okoeki.app.repository.AccountRepository;
+import com.kelvin.okoeki.app.repository.TransactionRepository;
+import com.kelvin.okoeki.app.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
+    private final TransactionRepository transactionRepository;
+
+    public Account save(Account account) {
+        account.setCreatedTime(new Timestamp(System.currentTimeMillis()));
+        accountRepository.save(account);
+        return accountRepository.findByAccountNumberEquals(account.getAccountNumber());
+    }
+
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
+
+    public Account findByAccountNumber(String accountNumber) {
+        Account account = accountRepository.findByAccountNumberEquals(accountNumber);
+        return account;
+    }
+
+
+    @Override
+    public Transaction transferFunds(
+            TransferBalanceRequest transferBalanceRequest
+    ) {
+        String fromAccountNumber = transferBalanceRequest.getFromAccountNumber();
+        String toAccountNumber = transferBalanceRequest.getToAccountNumber();
+        BigDecimal amount = transferBalanceRequest.getAmount();
+        Account fromAccount = accountRepository.findByAccountNumberEquals(
+                fromAccountNumber
+        );
+        Account toAccount = accountRepository.findByAccountNumberEquals(toAccountNumber);
+        if (fromAccount.getCurrentBalance().compareTo(BigDecimal.ONE) == 1
+                && fromAccount.getCurrentBalance().compareTo(amount) == 1
+        ) {
+            fromAccount.setCurrentBalance(fromAccount.getCurrentBalance().subtract(amount));
+            accountRepository.save(fromAccount);
+            toAccount.setCurrentBalance(toAccount.getCurrentBalance().add(amount));
+            accountRepository.save(toAccount);
+            Transaction transaction = transactionRepository.save(new Transaction(0L, fromAccountNumber, amount, new Timestamp(System.currentTimeMillis())));
+            return transaction;
+        }
+        return null;
+    }
+
+    public boolean withdraw(String accountNumber, double amount) {
+        Account account = accountRepository.findByAccountNumberEquals(accountNumber);
+        if (account != null && account.getCurrentBalance().doubleValue() >= amount) {
+            account.setCurrentBalance(BigDecimal.valueOf(account.getCurrentBalance().doubleValue() - amount));
+            accountRepository.save(account);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deposit(String accountNumber, double amount) {
+        Account account = accountRepository.findByAccountNumberEquals(accountNumber);
+        if (account != null) {
+            account.setCurrentBalance(BigDecimal.valueOf(account.getCurrentBalance().doubleValue() + amount));
+            accountRepository.save(account);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public AccountStatement getStatement(String accountNumber) {
+        Account account = accountRepository.findByAccountNumberEquals(accountNumber);
+        return new AccountStatement(account.getCurrentBalance(), transactionRepository.findByAccountNumberEquals(accountNumber));
+    }
 }
