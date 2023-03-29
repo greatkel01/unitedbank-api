f0c557d51f5c7d8f980de6dfebcfec828f5442db
Kelvin Okoeki <annabelogbuagu@Annabels-MacBook-Air.local>
2023-03-29 02:45:05 +0100

    Added JPA Repositories for the Model

--- src/main/java/com/kelvin/okoeki/app/repository/AccountRepository.java
+++ src/main/java/com/kelvin/okoeki/app/repository/AccountRepository.java
@@ -1,2 +1,11 @@
-package com.kelvin.okoeki.app.repository;public interface AccountRepository {
+package com.kelvin.okoeki.app.repository;
+
+import com.kelvin.okoeki.app.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByAccountNumberEquals(String accountNumber);
+
 }
--- src/main/java/com/kelvin/okoeki/app/repository/TransactionRepository.java
+++ src/main/java/com/kelvin/okoeki/app/repository/TransactionRepository.java
@@ -1,2 +1,13 @@
-package com.kelvin.okoeki.app.repository;public interface TransactionRepository {
+package com.kelvin.okoeki.app.repository;
+
+import com.kelvin.okoeki.app.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountNumberEquals(String accountNumber);
+
 }
